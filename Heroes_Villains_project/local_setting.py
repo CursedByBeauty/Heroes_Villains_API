d267e5061b1730fce64e170993542f0ee40da614
Justin Goncalves <jvgoncalves85@gmail.com>
2022-03-17 17:32:43 -0400

    Changed database setting + Created super_types + supers apps

---
+++ Heroes_Villains_project/local_setting.py
@@ -0,0 +1,21 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-4dy3jz-%@-rc^38=6@4@6$5er&jjrv$a3q^4ofw&4q)tx)$!b0'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'heroes_villians_database',
+        'USER': 'root',
+        'PASSWORD': 'Doghotman85!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+        'autocommit': True
+        }
+    }
+}
